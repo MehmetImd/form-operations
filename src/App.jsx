a14aa1a5790758dbd34e299f6291f683ffb943bd
Mehmet <mehmetimdat.py@gmail.com>
2024-03-16 21:21:25 +0300

    server2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"; 
+import { useState, useEffect } from "react"; 
 import BookCreate from "./components/BookCreate";
 import BookList from './components/BookList'
 import axios from "axios";
@@ -6,10 +6,24 @@ import axios from "axios";
 export const App = () => {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+      const response = await axios.get('http://localhost:3001/books');
+
+      setBooks(response.data)
+    };
+
+    useEffect(() => {
+      fetchBooks();
+    }, []);
+
+    const editBookById = async (id, newTitle) => {
+      const response = await axios.put(`http://localhost:3001/books/${id}`, {
+        title: newTitle
+      });
+
       const updatedBooks = books.map((book) => {
         if(book.id === id) {
-          return {...book, title: newTitle}
+          return {...book, ...response.data}
         };
         
         return book;
@@ -18,7 +32,9 @@ export const App = () => {
       setBooks(updatedBooks)
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+      axios.delete(`http://localhost:3001/books/${id}`);
+
       const updatedBooks = books.filter((book) => {
         return book.id !== id;
       });
@@ -31,12 +47,12 @@ export const App = () => {
           title
         });
 
-        const updatedBoobks = [
+        const updatedBooks = [
           ...books,
           response.data
         ];
 
-        setBooks(updatedBoobks);
+        setBooks(updatedBooks);
     };
 
   return (
